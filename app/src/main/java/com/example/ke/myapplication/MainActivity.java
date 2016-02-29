c6f13cc26fc3fadd6344c84c928b9a3c15645b3c
kezunkun <948226842@qq.com>
2016-02-29 20:46:12 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/ke/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ke/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //你妹的
+        //加一行
     }
 }
