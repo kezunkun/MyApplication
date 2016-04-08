b819be35bd4aa4e3ff7a51e47ed3320f1807734c
kezunkun <948226842@qq.com>
2016-04-08 17:15:28 +0800

    添加了文件的内容。。。

--- app/src/main/java/com/example/ke/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ke/myapplication/MainActivity.java
@@ -2,15 +2,40 @@ package com.example.ke.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private  static final String TAG="MainActivity";
+    private final int i = 6;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        Log.i(TAG,"abababababbababababbababab");
+       // test();
+        tv1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.println("aaaaaaaaaaaaaaa");
+                int i=9;
+                int j=i;
+                System.out.println();
+                test();
+            }
+        });
         //你妹的
         //加一行
         //测试数据行
+        
+    }
+    public  void test(){
+        //Log.d(TAG, "test: mytestsfkjsjfjkdshkfshdjfjhsf");
+        Log.i(TAG,"NINSJKJSDKJHJKJKSDNJKJFK");
     }
 }
