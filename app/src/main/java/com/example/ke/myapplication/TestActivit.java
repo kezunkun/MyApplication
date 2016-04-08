09ed7d831af9c0a9472d63969e1b6e7321b4171b
kezunkun <948226842@qq.com>
2016-04-08 15:33:40 +0800

    添加一个ACTIVITY

---
+++ app/src/main/java/com/example/ke/myapplication/TestActivit.java
@@ -0,0 +1,16 @@
+package com.example.ke.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ke on 2016/4/8.
+ */
+public class TestActivit extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //testactivity create
+    }
+}
